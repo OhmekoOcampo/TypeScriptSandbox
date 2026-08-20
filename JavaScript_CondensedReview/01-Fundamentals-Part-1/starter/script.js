f3b87fdcb55c6cab890f146b099b7bf0b457be57
OhmekoOcampo <ohmeko.ocampo@gmail.com>
2026-08-20 13:06:05 -0700

    Java Script Fundamentals - Part 1 - Data Types

---
+++ JavaScript_CondensedReview/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,20 @@
+let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+console.log(40 + 8 + 23 - 10);
+
+let country = "United States";
+let continent = "North America";
+let population = 349000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
