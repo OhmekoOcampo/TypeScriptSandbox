fdbf30d4a4bf2049bf43a2d72668795d91fd6c5f
OhmekoOcampo <ohmeko.ocampo@gmail.com>
2026-08-21 20:47:16 -0700

    Continued doing Javascript training, first challenge trying to do it.

--- JavaScript_CondensedReview/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript_CondensedReview/01-Fundamentals-Part-1/starter/script.js
@@ -2,8 +2,8 @@ let js = "amazing";
 if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
 
-let country = "United States";
-let continent = "North America";
+const country = "United States";
+const continent = "North America";
 let population = 349000000;
 
 console.log(country);
@@ -11,10 +11,38 @@ console.log(continent);
 console.log(population);
 
 let isIsland = false;
-let language;
+const language = "English";
 
 console.log(typeof country);
 console.log(typeof continent);
 console.log(typeof population);
 console.log(typeof isIsland);
 console.log(typeof language);
+
+//language = "Japanese";
+
+//Basic Operators
+halfPop = population / 2;
+console.log(halfPop);
+
+population++;
+console.log(population);
+
+finlandPop = 6000000;
+console.log("Is USA population greater than Finland population?");
+console.log(population > finlandPop);
+avgCountryPop = 33000000;
+console.log("Does USA have less people than the average country?");
+console.log(population < avgCountryPop);
+
+let state = "California";
+let description =
+  state +
+  " is in the " +
+  country +
+  " and its " +
+  population +
+  " million people speak " +
+  language;
+
+console.log(description);
