3dfafa5e39c9dd84e739b7a365734cba03caf3a2
Meko <ohmeko.ocampo@gmail.com>
2026-08-22 19:41:58 -0700

    Completed Coding Challenge #1

---
+++ JavaScript_CondensedReview/01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,13 @@
+let massMark = 78
+let heightMark = 1.69
+let massJohn = 92
+let heightJohn = 1.95
+
+let BMIMark = massMark / (heightMark**2)
+let BMIJohn = massJohn / (heightJohn**2)
+
+console.log(BMIMark)
+console.log(BMIJohn)
+
+let markHigherBMI = BMIMark > BMIJohn
+console.log(markHigherBMI)
