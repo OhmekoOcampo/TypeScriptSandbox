034bb09a1b399d6ca193a63282416941725dd67f
Meko <ohmeko.ocampo@gmail.com>
2026-09-18 09:51:17 -0700

    Playing with HTML buttons and Javascript.

--- JavaScript_CondensedReview/06-Modal/starter/script.js
+++ JavaScript_CondensedReview/06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+console.log(btnsOpenModal)
+console.log(3)
+
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+/*
+btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})*/
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
